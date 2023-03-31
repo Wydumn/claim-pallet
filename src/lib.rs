3e783f68266cd53c06aeb477a99b8cd4f5171242
Wydumn <971703596@qq.com>
2023-03-31 22:09:52 +0800

    poe module

---
+++ src/lib.rs
@@ -0,0 +1,113 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::{DispatchResultWithPostInfo, *};
+	use frame_system::{ensure_signed, pallet_prelude::*};
+	use sp_std::prelude::*;
+
+	// Config trait 用来配置pallet所必须的
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		// 存证（哈希值）长度限制
+		#[pallet::constant]
+		type MaxClaimLength: Get<u32>; // BoundedVec长度受限，接收u32长度的类型
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	// runtime不能使用vector类型，只能用BoundedVec
+	#[pallet::storage]
+	pub type Proofs<T: Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		BoundedVec<u8, T::MaxClaimLength>,
+		(T::AccountId, T::BlockNumber),
+	>;
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		ClaimCreated(T::AccountId, Vec<u8>),
+		ClaimRevoked(T::AccountId, Vec<u8>),
+		ClaimTransfered(T::AccountId, T::AccountId, Vec<u8>),
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		ProofAlreadyExist,
+		ClaimTooLong,
+		ClaimNotExist,
+		NotClaimOwner,
+	}
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(0)]
+		pub fn create_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+			let bounded_claim = BoundedVec::<u8, T::MaxClaimLength>::try_from(claim.clone())
+				.map_err(|_| Error::<T>::ClaimTooLong)?;
+			// 链上还没存过
+			ensure!(!Proofs::<T>::contains_key(&bounded_claim), Error::<T>::ProofAlreadyExist);
+
+			Proofs::<T>::insert(
+				&bounded_claim,
+				(sender.clone(), frame_system::Pallet::<T>::block_number()),
+			);
+
+			Self::deposit_event(Event::ClaimCreated(sender, claim));
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(0)]
+		pub fn revoke_claim(origin: OriginFor<T>, claim: Vec<u8>) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			let bounded_claim = BoundedVec::<u8, T::MaxClaimLength>::try_from(claim.clone())
+				.map_err(|_| Error::<T>::ClaimTooLong)?;
+			let (owner, _) = Proofs::<T>::get(&bounded_claim).ok_or(Error::<T>::ClaimNotExist)?;
+
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			Proofs::<T>::remove(&bounded_claim);
+
+			Self::deposit_event(Event::ClaimRevoked(sender, claim));
+
+			Ok(().into())
+		}
+
+		// allow the owner of a claim to transfer the claim
+		#[pallet::weight(0)]
+		pub fn transfer_claim(
+			origin: OriginFor<T>,
+			receiver: T::AccountId,
+			claim: Vec<u8>,
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+			let bounded_claim = BoundedVec::<u8, T::MaxClaimLength>::try_from(claim.clone())
+				.map_err(|_| Error::<T>::ClaimTooLong)?;
+			let (owner, block_number) = Proofs::<T>::get(&bounded_claim).ok_or(Error::<T>::ClaimNotExist)?;
+
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			Proofs::<T>::mutate(
+				&bounded_claim,
+				|val| {*val = Some((receiver.clone(), block_number))}
+			);
+
+      Self::deposit_event(Event::ClaimTransfered(sender, receiver, claim));
+      
+      Ok(().into())
+		}
+	}
+}
